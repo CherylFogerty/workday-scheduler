304f09ce3ecf32398050ae9fcff6aeffc3e95e2e
Cheryl Fogerty <cfogerty@aol.com>
2021-01-24 21:35:20 -0600

    add color coding updates

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ console.log(hour)
 
 
 })
+
+function colorupdater (){
+    var currentTime = moment().hours()
+    $(".row").each(function() {
+    var tabTime = parseInt($(this).attr("data-time"))
+        if(tabTime < currentTime){
+            $(this).addClass("past")
+        }
+        else if (tabTime === currentTime){
+            $(this).removeClass("past")
+            $(this).addClass("present")
+  }
+        else {
+            $(this).removeClass("past")
+            $(this).addClass("present")
+            $(this).addClass("future")
+        }
+    })
+
+}
+colorupdater()
+
+
+localStorage.setItem("time-input", "save");
+const save = localStorage.getItem("time-input")
